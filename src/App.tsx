24b3c188eb5c3c987a6fb57378a9664b2540b2ca
henryenterprise <henrysonsocial@gmail.com>
2026-08-26 00:03:19 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Admin from './pages/Admin';
 import Index from './pages/Index';
 import Login from './pages/Login';
 import Register from './pages/Register';
